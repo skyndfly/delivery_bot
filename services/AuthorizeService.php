fa330eee6d47b45ae3a277cce18b7da1b1a2a79d
skyndfly <skyandy2796@yandex.ru>
2025-11-22 23:29:51 +0300

    белый список и недоступность сервиса в определнное время

--- services/AuthorizeService.php
+++ services/AuthorizeService.php
@@ -2,21 +2,44 @@
 
 namespace services;
 
+use DateTimeImmutable;
+use DateTimeZone;
+use DomainException;
 use repositories\contracts\UserRepositoryContract;
-use repositories\UserRedisRepository;
 
 class AuthorizeService
 {
     private UserRepositoryContract $userRepository;
-
+    private array $whiteList = [
+        1535637656,
+        595913846
+    ];
     public function __construct(UserRepositoryContract $userRepository)
     {
         $this->userRepository = $userRepository;
     }
 
-    public function handle(int $userId): bool
+    public function handle(int $userId, $chatId): bool
     {
+        if(in_array($chatId, $this->whiteList)){
+            return true;
+        }
+        if (!$this->isServiceAvailable()){
+            throw new DomainException('Сервис недоступен в период с 16:00 до 23:59');
+        }
         return $this->userRepository->exists($userId);
+    }
+
+    private function isServiceAvailable(): bool
+    {
+        $currentTime = new DateTimeImmutable('now', new DateTimeZone('Europe/Moscow'));
+        $currentHour = (int)$currentTime->format('H');
+        $currentMinute = (int)$currentTime->format('i');
+
+        // Текущее время в минутах от начала дня
+        $currentTimeMinutes = $currentHour * 60 + $currentMinute;
+        // Проверяем период с 16:00 (960 минут) до 23:59 (1439 минут)
+        return $currentTimeMinutes < 16 * 60 || $currentTimeMinutes > 23 * 60 + 59;
     }
 
 }
